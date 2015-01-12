cc9ccd20b277c63ccbb661545f4ac69509f9151e
J. Andrew Brassington <jabbrass@zoho.com>
2015-01-12 14:50:31 -0800

    Update Config.js

--- Config.js
+++ Config.js
@@ -23,7 +23,7 @@
 define(function (require, exports, module) {
 	module.exports = {
 		NAME: 'websiteduck.wdminimap',
-		MINIMAP_WIDTH: 60,
+		MINIMAP_WIDTH: 50,
 		defaultPreferences: { 
 			enabled: true, 
 			type: 'codemirror' 
