758ba626d84ef00b6d71add339cb26dbe548ba85
J. Andrew Brassington <jabbrass@zoho.com>
2015-01-12 16:29:58 -0800

    Cut width of minimap by 1/2

--- Config.js
+++ Config.js
@@ -23,7 +23,7 @@
 define(function (require, exports, module) {
 	module.exports = {
 		NAME: 'websiteduck.wdminimap',
-		MINIMAP_WIDTH: 65,
+		MINIMAP_WIDTH: 35,
 		defaultPreferences: { 
 			enabled: true, 
 			type: 'codemirror' 
